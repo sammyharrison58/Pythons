385307175b3c4630189f3d621e3f15cdc7c07497
sammyharrison58 <sammyharrison58@gmail.com>
2025-12-17 23:42:38 +0300

    apply changes

---
+++ game2.py
@@ -0,0 +1,29 @@
+import sys
+import pygame
+from pygame.locals import *
+
+pygame.init()
+surface = pygame.display.set_mode((560, 580))
+clock = pygame.time.Clock()
+surfrect = surface.get_rect()
+rect = pygame.Rect((1, 4), (200, 200))
+rect.center = (surfrect.w / 2, surfrect.h / 2)
+touched = False
+while True:
+
+    for ev in pygame.event.get():
+        if ev.type == QUIT:
+            pygame.quit()
+        elif ev.type == pygame.MOUSEBUTTONDOWN:
+            if rect.collidepoint(ev.pos):
+                touched = True
+                pygame.mouse.get_rel()
+            elif ev.type == pygame.MOUSEBUTTONUP:
+                touched = False
+            clock.tick(60)
+            surface.fill((0, 0, 0))
+            if touched:
+                rect.move_ip(pygame.mouse.get_rel())
+                rect.clamp_ip(surfrect)
+                surface.fill((255, 255, 255), rect)
+                pygame.display.flip()
